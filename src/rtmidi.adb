55b6903ef120bc34685fd62f41dbb94360545245
Hervé ROMANO <shuihuzhuan@free.fr>
2024-06-03 09:33:07 +0200

    Renames rtmidi.adb internals
    Signed-off-by: Hervé ROMANO <shuihuzhuan@free.fr>

--- src/rtmidi.adb
+++ src/rtmidi.adb
@@ -8,10 +8,10 @@ package body Rtmidi is
 
       use Interfaces.C.Strings;
 
-      function rtmidi_api_name (api : RtMidiApi) return chars_ptr with
+      function Internal (api : RtMidiApi) return chars_ptr with
         Import => True, Convention => C, External_Name => "rtmidi_api_name";
 
-      name : constant String := Value (rtmidi_api_name (api));
+      name : constant String := Value (Internal (api));
    begin
       return name;
    end api_name;
@@ -21,11 +21,11 @@ package body Rtmidi is
 
       use Interfaces.C.Strings;
 
-      function rtmidi_api_display_name (api : RtMidiApi) return chars_ptr with
+      function Internal (api : RtMidiApi) return chars_ptr with
         Import        => True, Convention => C,
         External_Name => "rtmidi_api_display_name";
 
-      name : constant String := Value (rtmidi_api_display_name (api));
+      name : constant String := Value (Internal (api));
    begin
       return name;
    end api_display_name;
@@ -35,13 +35,13 @@ package body Rtmidi is
 
       use Interfaces.C.Strings;
 
-      function rtmidi_compiled_api_by_name
+      function Internal
         (name : chars_ptr) return RtMidiApi with
         Import        => True, Convention => C,
         External_Name => "rtmidi_compiled_api_by_name";
 
       api : constant RtMidiApi :=
-        rtmidi_compiled_api_by_name (New_String (name));
+        Internal (New_String (name));
    begin
       return api;
    end compiled_api_by_name;
@@ -54,14 +54,14 @@ package body Rtmidi is
       use Interfaces.C;
       use Interfaces.C.Strings;
 
-      procedure rtmidi_open_port
+      procedure Internal
         (device     : in out RtMidiPtr;
          portNumber : unsigned; -- if 0, the first available.
          portName   : chars_ptr) with
         Import => True, Convention => C, External_Name => "rtmidi_open_port";
 
    begin
-      rtmidi_open_port (device, unsigned (number), New_String (name));
+      Internal (device, unsigned (number), New_String (name));
    end open_port;
 
    ----------------------------------------------------------------------------
@@ -69,23 +69,23 @@ package body Rtmidi is
 
       use Interfaces.C.Strings;
 
-      procedure rtmidi_open_virtual_port
+      procedure Internal
         (device : in out RtMidiPtr; portName : chars_ptr) with
         Import        => True, Convention => C,
         External_Name => "rtmidi_open_virtual_port";
 
    begin
-      rtmidi_open_virtual_port (device, New_String (name));
+      Internal (device, New_String (name));
    end open_virtual_port;
 
    ----------------------------------------------------------------------------
    procedure close_port (device : in out RtMidiPtr) is
 
-      procedure rtmidi_close_port (device : in out RtMidiPtr) with
+      procedure Internal (device : in out RtMidiPtr) with
         Import => True, Convention => C, External_Name => "rtmidi_close_port";
 
    begin
-      rtmidi_close_port (device);
+      Internal (device);
    end close_port;
 
    ----------------------------------------------------------------------------
@@ -93,12 +93,12 @@ package body Rtmidi is
 
       use Interfaces.C;
 
-      function rtmidi_get_port_count (device : RtMidiPtr) return unsigned with
+      function Internal (device : RtMidiPtr) return unsigned with
         Import        => True, Convention => C,
         External_Name => "rtmidi_get_port_count";
 
    begin
-      return Natural (rtmidi_get_port_count (device));
+      return Natural (Internal (device));
    end port_count;
 
    ----------------------------------------------------------------------------
@@ -109,7 +109,7 @@ package body Rtmidi is
       use Interfaces.C;
       use Interfaces.C.Strings;
 
-      function rtmidi_get_port_name
+      function Internal
         (device :     RtMidiPtr; portNumber : unsigned; bufOut : chars_ptr;
          bufLen : out int) return int with
         Import        => True, Convention => C,
@@ -120,14 +120,14 @@ package body Rtmidi is
       name   : chars_ptr;
    begin
       result :=
-        rtmidi_get_port_name (device, unsigned (number), Null_Ptr, buflen);
+        Internal (device, unsigned (number), Null_Ptr, buflen);
 
       if result < 0 then
          return "";
       end if;
 
       name   := New_String ((1 .. Integer (buflen) => ' '));
-      result := rtmidi_get_port_name (device, unsigned (number), name, buflen);
+      result := Internal (device, unsigned (number), name, buflen);
 
       if result <= 0 then
          return "";
@@ -145,12 +145,12 @@ package body Rtmidi is
       use Interfaces.C;
       use Interfaces.C.Strings;
 
-      function rtmidi_get_compiled_api
+      function Internal_Get
         (apis : in out RtMidiApi_Array; apis_size : unsigned) return int with
         Import        => True, Convention => C,
         External_Name => "rtmidi_get_compiled_api";
 
-      function get_num_compiled_api
+      function Internal_Get_Num
         (apis : chars_ptr; apis_size : unsigned) return int with
         Import        => True, Convention => C,
         External_Name => "rtmidi_get_compiled_api";
@@ -159,7 +159,7 @@ package body Rtmidi is
       ret  : int      := 0;
 
    begin
-      ret := get_num_compiled_api (Null_Ptr, 0);
+      ret := Internal_Get_Num (Null_Ptr, 0);
 
       if ret <= 0 then
          declare
@@ -174,7 +174,7 @@ package body Rtmidi is
       declare
          result : RtMidiApi_Array (1 .. Integer (size));
       begin
-         ret := rtmidi_get_compiled_api (result, size);
+         ret := Internal_Get (result, size);
          if ret <= 0 then
             declare
                result : RtMidiApi_Array (1 .. 0);
